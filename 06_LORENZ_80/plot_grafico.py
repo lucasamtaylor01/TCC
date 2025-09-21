c87c8bbd1e7c23a8f4fbded059b42dfc5a232170
lucasamtaylor01 <lucasamtaylor@usp.br>
2025-09-21 20:32:24 -0300

    last update

--- 06_LORENZ_80/plot_grafico.py
+++ 06_LORENZ_80/plot_grafico.py
@@ -7,7 +7,7 @@ BASE = Path(__file__).resolve().parent
 DATA = BASE / "data" / "python01.csv"
 OUTDIR = BASE / "src"
 OUTDIR.mkdir(parents=True, exist_ok=True)
-OUTPNG = OUTDIR / "evolucao_xyz_python327.png"
+OUTPNG = OUTDIR / "evolucao_xyz_python01.png"
 
 # Ler CSV
 df = pd.read_csv(DATA)
@@ -19,12 +19,13 @@ z1 = df["z1"]
 
 # Plot
 plt.figure(figsize=(12, 8), dpi=300)
-plt.plot(t, x1, label="$x_1$", linewidth=1.5, color="green")
+#plt.plot(t, x1, label="$x_1$", linewidth=1.5, color="green")
 plt.plot(t, y1, label="$y_1$", linewidth=1.5, color="blue")
 plt.plot(t, z1, label="$z_1$", linewidth=1.5, color="red")
 
 plt.xlabel("t (dias)", fontsize=16)
 plt.ylabel("Valores", fontsize=16)
+plt.ylim(-0.4,0.6)
 plt.title("Evolução temporal de $x_1$, $y_1$ e $z_1$", fontsize=18)
 plt.legend(fontsize=14)
 plt.grid(True)
